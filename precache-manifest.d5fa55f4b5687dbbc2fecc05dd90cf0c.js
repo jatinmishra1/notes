1009c4dec8da9d459ebfce1c57fba423b25ea94e
jatin mishra <balajimahraaj123@gmail.com>
2021-12-10 17:24:47 -0500

    Updates

---
+++ precache-manifest.d5fa55f4b5687dbbc2fecc05dd90cf0c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "21262b5fa8b93c4fecf8",
+    "url": "/notes/static/css/main.720286a2.chunk.css"
+  },
+  {
+    "revision": "21262b5fa8b93c4fecf8",
+    "url": "/notes/static/js/main.0ea0c190.chunk.js"
+  },
+  {
+    "revision": "01155016b6fc9fe91e5f",
+    "url": "/notes/static/js/runtime~main.21ba3d73.js"
+  },
+  {
+    "revision": "d49d7230978ef85c0187",
+    "url": "/notes/static/css/2.eb1fa8d4.chunk.css"
+  },
+  {
+    "revision": "d49d7230978ef85c0187",
+    "url": "/notes/static/js/2.4fd6d2ce.chunk.js"
+  },
+  {
+    "revision": "3570feaef6bba048000716b1a072e872",
+    "url": "/notes/static/media/1.3570feae.jpg"
+  },
+  {
+    "revision": "8784cb962b0593b5efb51100b12ec2ed",
+    "url": "/notes/static/media/3.8784cb96.jpg"
+  },
+  {
+    "revision": "fffaf225002e4f53d83333540fcadcf7",
+    "url": "/notes/static/media/5.fffaf225.jpg"
+  },
+  {
+    "revision": "f35a158e545cb9f3d52a367692e597aa",
+    "url": "/notes/static/media/4.f35a158e.jpg"
+  },
+  {
+    "revision": "0299be3cd9e2bde3debb8862573c4141",
+    "url": "/notes/index.html"
+  }
+];
